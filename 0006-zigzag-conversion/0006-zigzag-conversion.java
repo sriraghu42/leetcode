6bf0003f76ee54897b30a973b4f8e49c71dfb56a
sriraghu42 <127194640+sriraghu42@users.noreply.github.com>
2024-10-11 13:25:15 -0400

    Time: 5 ms (69.14%), Space: 45.2 MB (53.62%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,16 +1,16 @@
 class Solution {
     public String convert(String s, int numRows) {
         if(numRows==1) return s;
-        List<List<Character>> list = new ArrayList<>();
+        List<StringBuilder> list = new ArrayList<>();
         for(int i=0; i<numRows; i++){
-            list.add(new ArrayList<>());
+            list.add(new StringBuilder());
         }
         int k = 0;
         boolean topToBottom = true;
         int i=0;
         while(k<s.length()){
             char ch = s.charAt(k);
-            list.get(i).add(ch);
+            list.get(i).append(ch);
             if(i==0){
                 topToBottom = true;
                 i++;
@@ -24,10 +24,8 @@ class Solution {
             k++;
         }
         StringBuilder sb = new StringBuilder();
-        for(List<Character> l : list){
-            for(char ch : l){
-                sb.append(ch);
-            }
+        for(StringBuilder builder: list){
+                sb.append(builder);
         }
         return sb.toString();
     }
