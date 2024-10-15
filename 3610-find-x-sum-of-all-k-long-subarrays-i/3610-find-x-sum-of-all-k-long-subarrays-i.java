1e2663088b1fe6415201a320c0ec319dde52c2d4
sriraghu42 <127194640+sriraghu42@users.noreply.github.com>
2024-10-15 14:18:54 -0400

    Time: 14 ms (37.6%), Space: 44.8 MB (94.38%) - LeetHub

--- 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.java
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.java
@@ -1,29 +1,38 @@
 class Solution {
     public int[] findXSum(int[] nums, int k, int x) {
-        Queue<int[]> pq = new PriorityQueue<>((a,b) -> {
-            int comp = a[1]-b[1];
-            if(comp!=0) return comp;
-            return a[0]-b[0];
-        });
-        int[] ans = new int[nums.length-k+1];
         Map<Integer,Integer> map = new HashMap<>();
+        int[] ans = new int[nums.length-k+1];
+        int counter=0;
         int i = 0;
-        int j = 0;
-        while(j<nums.length){
+        for(int j=0; j<nums.length; j++){
             map.put(nums[j],map.getOrDefault(nums[j],0)+1);
             if(j-i+1==k){
-                for(Map.Entry<Integer,Integer> entry : map.entrySet()){
-                    pq.offer(new int[] {entry.getKey(),entry.getValue()});
-                    if(pq.size()>x) pq.poll();
+                List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
+                Collections.sort(list, (a, b) -> {
+                int valueComparison = b.getValue().compareTo(a.getValue());
+                if (valueComparison != 0) {
+                    return valueComparison;
+                } else {
+                    return b.getKey().compareTo(a.getKey());
                 }
-                while(!pq.isEmpty()){
-                    int[] entry = pq.poll();
-                    ans[i]+=entry[0]*entry[1];
+                });
+                int sum=0;
+                if(list.size()<x){
+                    for(int c=i; c<=j; c++){
+                        sum+=nums[c];
+                    }
                 }
+                else{
+                    for(int m=0; m<x; m++){
+                    sum+=list.get(m).getKey()*list.get(m).getValue();
+                    }
+                }
+                ans[counter] = sum;
+                counter++;
                 map.put(nums[i],map.get(nums[i])-1);
                 i++;
             }
-            j++;
+
         }
         return ans;
     }
