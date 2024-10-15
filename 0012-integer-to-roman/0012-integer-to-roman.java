9f1dfeaf95b767f330f7b16689899ab978b1a799
sriraghu42 <127194640+sriraghu42@users.noreply.github.com>
2024-10-15 17:47:50 -0400

    Time: 6 ms (25.15%), Space: 44.4 MB (52.79%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -16,30 +16,16 @@ class Solution {
         map.put(1000,"M");
         int[] nums = new int[] {1,4,5,9,10,40,50,90,100,400,500,900,1000};
         StringBuilder sb = new StringBuilder();
-        String n = String.valueOf(num);
-        for(int i=0; i<n.length(); i++){
-            int number =  (n.charAt(i)-'0') * (int) Math.pow(10,n.length()-i-1);
-            while(number>0){
-                if(number>=nums[nums.length-1]){
-                    sb.append("M");
-                    number-=1000;
-                }
-                else if(number==nums[0]){
-                    sb.append("I");
-                    number-=1;
-                }
-                else{
-                    int d = 0 ;
-                    int e = nums.length-1;
-                    while(d<=e){
-                        int f = d + (e-d)/2;
-                        if(nums[f]<=number) d=f+1;
-                        else e=f-1;
-                    }
-                    sb.append(map.get(nums[e]));
-                    number-=nums[e];
-                }
+        while(num>0){
+            int d = 0 ;
+            int e = nums.length-1;
+            while(d<=e){
+                int f = d + (e-d)/2;
+                if(nums[f]<=num) d=f+1;
+                else e=f-1;
             }
+            sb.append(map.get(nums[e]));
+            num-=nums[e];
         }
         return sb.toString();
     }
