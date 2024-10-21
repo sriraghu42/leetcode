0adc42b7f9eb98e9f09bcccfd0bf72573eb1572b
sriraghu42 <127194640+sriraghu42@users.noreply.github.com>
2024-10-21 03:17:36 -0400

    Time: 1 ms (99.68%), Space: 44.9 MB (34.2%) - LeetHub

--- 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
+++ 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
@@ -1,24 +1,21 @@
 class Solution {
     public int[] kWeakestRows(int[][] mat, int k) {
         Queue<int[]> pq = new PriorityQueue<>((a,b) -> {
-            int comp = b[1]-a[1];
-            if(comp!=0) return comp;
-            return b[0]-a[0];
-        });
-        
+            int compValue = b[1]-a[1];
+            if(compValue!=0) return compValue;
+            return b[0]-a[0];});
         for(int i=0; i<mat.length; i++){
-            int[] row = mat[i];
-            int m=0;
-            int n=row.length-1;
+            int[] arr = mat[i];
+            int m = 0;
+            int n = arr.length-1;
             while(m<=n){
                 int p = m+(n-m)/2;
-                if(row[p]==1) m=p+1;
+                if(arr[p]==1) m=p+1;
                 else n=p-1;
             }
-            pq.offer(new int[] {i,m});
+            pq.offer(new int[] {i,n+1});
             if(pq.size()>k) pq.poll();
         }
-
         int[] ans = new int[k];
         for(int i=k-1; i>=0; i--){
             ans[i] = pq.poll()[0];
