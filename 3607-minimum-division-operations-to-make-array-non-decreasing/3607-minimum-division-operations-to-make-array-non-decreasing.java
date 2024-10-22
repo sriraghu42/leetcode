f19e5ee8b194b5ff753ee4c0ffac0ceb4daae440
sriraghu42 <127194640+sriraghu42@users.noreply.github.com>
2024-10-21 23:51:57 -0400

    Time: 411 ms (40.47%), Space: 57.8 MB (86.77%) - LeetHub

--- 3607-minimum-division-operations-to-make-array-non-decreasing/3607-minimum-division-operations-to-make-array-non-decreasing.java
+++ 3607-minimum-division-operations-to-make-array-non-decreasing/3607-minimum-division-operations-to-make-array-non-decreasing.java
@@ -1,23 +1,20 @@
 class Solution {
     public int minOperations(int[] nums) {
-        int counter=0;
-        for(int i=0; i<nums.length-1; i++){
-            if(i>=0 && nums[i]<=nums[i+1]) continue;
-            while(i>=0 && nums[i]>nums[i+1]){
+        int count = 0;
+        for(int i=nums.length-2; i>=0; i--){
+            while(nums[i]>nums[i+1]){
                 int div = getDivisor(nums[i]);
-                //System.out.println(div);
-                if(div==-1) return -1;
-                counter++;
+                if(div == -1) return -1;
                 nums[i] = nums[i]/div;
-                i--;
-            }
+                count++;
+            }  
         }
-        return counter;
+        return count;
     }
 
     public int getDivisor(int num){
-        for(int i=2; i<= (int) Math.sqrt(num); i++){
-            if(num % i == 0) return num/i;
+        for(int i=2; i<=Math.sqrt(num); i++){
+            if(num%i==0) return num/i;
         }
         return -1;
     }
