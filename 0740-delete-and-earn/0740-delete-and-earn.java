a34c4b70c6439f506bd839372ad7c05d2115548a
sriraghu42 <127194640+sriraghu42@users.noreply.github.com>
2024-10-26 15:50:09 -0400

    Time: 6 ms (58.74%), Space: 44.2 MB (74.56%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.java
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -1,19 +1,22 @@
 class Solution {
     public int deleteAndEarn(int[] nums) {
         Arrays.sort(nums);
-        Map<Integer,Integer> map = new HashMap<>();
+        //Map<Integer,Integer> map = new HashMap<>();
+        int[] arr = new int[(int) Math.pow(10,4)+1];
         for(int num : nums){
-            map.put(num,map.getOrDefault(num,0)+1);
+            //map.put(num,map.getOrDefault(num,0)+1);
+            arr[num]++;
         }
         int min = nums[0];
         int max = nums[nums.length-1];
         int[] dp = new int[max+1];
-        dp[min] = min * map.get(min);
+        dp[min] = min * arr[min];
         if(max>min){
             //dp[min+1] = Math.max(dp[min],(min+1)*map.get(min+1));
             for(int i=min+1; i<=max; i++){
-                if(map.containsKey(i)) dp[i] = Math.max(dp[i-1],dp[i-2]+i*map.get(i));
-                else dp[i] = dp[i-1];
+                dp[i] = Math.max(dp[i-1],dp[i-2]+i*arr[i]);
+                //if(map.containsKey(i)) dp[i] = Math.max(dp[i-1],dp[i-2]+i*map.get(i));
+                //else dp[i] = dp[i-1];
             }
         }
         return dp[max];
