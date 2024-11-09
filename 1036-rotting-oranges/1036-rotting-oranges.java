417dfe40d79e923ec1c030769c218048ca136f67
sriraghu42 <127194640+sriraghu42@users.noreply.github.com>
2024-11-09 02:50:38 -0500

    Time: 4 ms (8.26%), Space: 42.7 MB (23.89%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -4,40 +4,33 @@ class Solution {
         int cols = grid[0].length;
         Queue<int[]> q = new LinkedList<>();
         int counter = 0;
+        int freshOranges = 0;
         for(int i=0; i<rows; i++){
             for(int j=0; j<cols; j++){
-                if(grid[i][j]==2){
-                    q.offer(new int[] {i-1,j});
-                    q.offer(new int[] {i+1,j});
-                    q.offer(new int[] {i,j-1});
-                    q.offer(new int[] {i,j+1});
-                }
+                if(grid[i][j]==2) q.offer(new int[] {i,j});
+                if(grid[i][j]==1) freshOranges++;
             }
         }
+        if(freshOranges==0) return 0;
+        int[][] directions = new int[][] {{-1,0},{0,1},{1,0},{0,-1}};
         boolean first = true;
         while(!q.isEmpty()){
             int size = q.size();
             if(!first) counter++;
             for(int m=0; m<size; m++){
                 int[] cell = q.poll();
-                int a = cell[0];
-                int b = cell[1];
-                if(a<0 || a>=rows || b<0 || b>=cols) continue;
-                if(grid[a][b] == 0 || grid[a][b]==2) continue;
-                grid[a][b] = 2;
-                q.add(new int[] {a-1,b});
-                q.add(new int[] {a+1,b});
-                q.add(new int[] {a,b-1});
-                q.add(new int[] {a,b+1});
+                for(int[] dir : directions){
+                    int a = cell[0] + dir[0];
+                    int b = cell[1] + dir[1];
+                    if(a<0 || a>=rows || b<0 || b>=cols) continue;
+                    if(grid[a][b] == 0 || grid[a][b]==2) continue;
+                    grid[a][b] = 2;
+                    q.add(new int[] {a,b});
+                    freshOranges--;
+                }
                 first = false;
             }
         }
-
-        for(int i=0; i<rows; i++){
-            for(int j=0; j<cols; j++){
-                if(grid[i][j]==1) return -1;
-            }
-        }
-        return counter;    
+        return freshOranges==0?counter:-1;    
     }
 }
