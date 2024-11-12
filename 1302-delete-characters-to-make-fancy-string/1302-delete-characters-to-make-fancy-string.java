173f16a3ade8610328c0cb13163280e12ef603ef
sriraghu42 <127194640+sriraghu42@users.noreply.github.com>
2024-11-12 17:53:02 -0500

    Time: 33 ms (69.62%), Space: 46.1 MB (5.15%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.java
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.java
@@ -1,21 +1,15 @@
 class Solution {
     public String makeFancyString(String s) {
-        int counter = 1;
         StringBuilder sb = new StringBuilder();
         sb.append(s.charAt(0));
-        int i = 1;
-        while(i<s.length()){
-            if(s.charAt(i)==s.charAt(i-1)){
-                counter++;
-                if(counter<3) sb.append(s.charAt(i)); 
-
-            }
-            else{
-                sb.append(s.charAt(i));
-                counter=1;
-            }
-            i++;
+        int j=1;
+        int counter = 1;
+        while(j<s.length()){
+            if(s.charAt(j)==s.charAt(j-1)) counter++;
+            else counter = 1;
+            if(counter<3) sb.append(s.charAt(j));
+            j++;
         }
-    return sb.toString();
+        return sb.toString();
     }
 }
