7a153ecdbf066b6118f40b917e98b5b15045587f
sriraghu42 <127194640+sriraghu42@users.noreply.github.com>
2024-11-15 22:51:10 -0500

    Time: 1 ms (100%), Space: 46 MB (6.12%) - LeetHub

--- 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.java
+++ 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.java
@@ -1,18 +1,20 @@
 class Solution {
     public int[] resultsArray(int[] nums, int k) {
+        if(k==1) return nums;
         int[] ans = new int[nums.length-k+1];
         Arrays.fill(ans,-1);
-        for(int i=0; i<=nums.length-k; i++){
-            if(checkOrder(nums,k,i)) ans[i] = nums[i+k-1];
-            else ans[i] = -1;
+        int i = 0;
+        int j = 1;
+        while(j<nums.length){
+            if(nums[j]!=nums[j-1]+1) i = j;
+            else{
+                if(j-i+1==k){
+                    ans[i] = nums[j];
+                    i++;
+                }
+            }
+            j++;
         }
         return ans;
-    }
-
-    public boolean checkOrder(int[] nums, int k, int m){
-        for(int i=m+1; i<m+k; i++){
-            if(nums[i]!=nums[i-1]+1) return false;
-        }
-        return true;
     }
 }
