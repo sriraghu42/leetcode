73a6f185e58066c06ad277f49f1b1bf61d54ea3a
sriraghu42 <127194640+sriraghu42@users.noreply.github.com>
2024-11-21 17:10:57 -0500

    Time: 19 ms (93.1%), Space: 78.8 MB (6.02%) - LeetHub

--- 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.java
+++ 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.java
@@ -1,5 +1,4 @@
 class Solution {
-
     public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
         char[][] grid = new char[m][n];
         for(int[] guard : guards){
@@ -8,31 +7,30 @@ class Solution {
         for(int[] wall : walls){
             grid[wall[0]][wall[1]] = 'W';
         }
-        boolean[][] marked = new boolean[m][n];
         for(int[] guard : guards){
             int i = guard[0];
             int j = guard[1];
             for(int k=i+1;k<m;k++){
                 if(grid[k][j]=='G' || grid[k][j]=='W') break;
-                marked[k][j] = true;
+                grid[k][j] = 'M';
             }
             for(int k=i-1;k>=0;k--){
                 if(grid[k][j]=='G' || grid[k][j]=='W') break;
-                marked[k][j] = true;
+                grid[k][j] = 'M';
             }
             for(int k=j+1;k<n;k++){
                 if(grid[i][k]=='G' || grid[i][k]=='W') break;
-                marked[i][k] = true;
+                grid[i][k] = 'M';
             }
             for(int k=j-1;k>=0;k--){
                 if(grid[i][k]=='G' || grid[i][k]=='W') break;
-                marked[i][k] = true;
+                grid[i][k] = 'M';
             }
         }
         int counter = 0;
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(grid[i][j]!='G' && grid[i][j]!='W' && !marked[i][j]) counter++;
+                if(grid[i][j]==0) counter++;
             }
         }
         return counter;
