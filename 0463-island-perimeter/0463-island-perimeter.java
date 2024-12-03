e9fdd43528aaeb32f80eedb0f83eee6af85a5701
sriraghu42 <127194640+sriraghu42@users.noreply.github.com>
2024-12-02 19:22:15 -0500

    Time: 8 ms (29.43%), Space: 44.8 MB (98.4%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -1,17 +1,26 @@
 class Solution {
-    int[][] directions = new int[][]{{-1,0},{1,0},{0,1},{0,-1}};
+    int count = 0;
+    int[][] directions = new int[][] {{-1,0},{1,0},{0,1},{0,-1}};
     public int islandPerimeter(int[][] grid) {
-        int count = 0;
         for(int i=0; i<grid.length; i++){
             for(int j=0; j<grid[0].length; j++){
-                if(grid[i][j]==0) continue;
-                for(int[] dir : directions){
-                    int x = i+dir[0];
-                    int y = j+dir[1];
-                    if(x<0 || y<0 || x>=grid.length || y>=grid[0].length || grid[x][y]==0) count++;
+                if(grid[i][j] == 1){
+                    dfs(grid,i,j);
+                    break;
                 }
             }
         }
         return count;
+    }
+
+    public void dfs(int[][] grid, int i, int j){
+        grid[i][j] = 2;
+        for(int[] dir : directions){
+            int x = dir[0] + i;
+            int y = dir[1] + j;
+            if(x<0 || y<0 || x>=grid.length || y>=grid[0].length || grid[x][y]==0) count++;
+            else if(grid[x][y]==1) dfs(grid,x,y);
+            else continue;
+        }
     }
 }
