547e26c00207bf84565fb6faf97ed9cc8dd27b30
sriraghu42 <127194640+sriraghu42@users.noreply.github.com>
2024-12-04 20:26:53 -0500

    Time: 0 ms (100%), Space: 45.8 MB (12.26%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,30 +2,19 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int rows = matrix.length;
         int cols = matrix[0].length;
-        int top = 0;
-        int bottom = rows - 1;
-        int left = 0;
-        int right = cols - 1;
-        boolean[][] visited = new boolean[rows][cols];
+        boolean[] zeroRows = new boolean[rows];
+        boolean[] zeroCols = new boolean[cols];
         for(int i=0; i<rows; i++){
             for(int j=0; j<cols; j++){
-                if(matrix[i][j]==0 && !visited[i][j]) helper(matrix,i,j,left,right,bottom,top,visited);
+                if(matrix[i][j]==0){
+                    zeroRows[i] = true;
+                    zeroCols[j] = true;
+                }
             }
         }
-    }
-
-    public void helper(int[][] matrix, int i, int j, int left, int right, int bottom, int top, boolean[][] visited){
-        visited[i][j] = true;
-        for(int m=top; m<=bottom; m++){
-            if(matrix[m][j]!=0){
-                matrix[m][j] = 0;
-                visited[m][j] = true;
-            }
-        }
-        for(int m=left; m<=right; m++){
-            if(matrix[i][m]!=0){
-                matrix[i][m] = 0;
-                visited[i][m] = true;
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++){
+                if(zeroRows[i] || zeroCols[j]) matrix[i][j] = 0;
             }
         }
     }
