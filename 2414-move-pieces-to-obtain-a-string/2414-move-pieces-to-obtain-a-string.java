3ef73f83ba51d2ccc257e926ab3241a4023dd61f
sriraghu42 <127194640+sriraghu42@users.noreply.github.com>
2024-12-08 20:25:23 -0500

    Time: 15 ms (46.09%), Space: 45.5 MB (73.14%) - LeetHub

--- 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.java
+++ 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.java
@@ -1,15 +1,5 @@
 class Solution {
     public boolean canChange(String start, String target) {
-        int[] count1 = new int[2];
-        int[] count2 = new int[2];
-        for(int i=0; i<start.length(); i++){
-            if(start.charAt(i)=='L') count1[0]++;
-            else if(start.charAt(i)=='R') count1[1]++;
-            else ;
-            if(target.charAt(i)=='L') count2[0]++;
-            else if(target.charAt(i)=='R') count2[1]++;
-        }
-        if(count1[0]!=count2[0] || count1[1]!=count2[1]) return false;
         int i = 0;
         int j = 0;
         int n = start.length();
@@ -18,14 +8,20 @@ class Solution {
             while(j<n && target.charAt(j)=='_') j++;
             if(i>=n && j>=n) return true;
             if(i>=n || j>=n) return false;
-            char a = start.charAt(i);
-            char b = target.charAt(j);
-            if(a!=b) return false;
-            if(a=='L' && i<j) return false;
-            if(a=='R' && i>j) return false;
+            if(start.charAt(i)!=target.charAt(j)) return false;
+            char ch = start.charAt(i);
+            if(ch=='L' && i<j) return false;
+            if(ch=='R' && i>j) return false;
             i++;
             j++;
         }
-        return true;
+        if(i>=n){
+            while(j<n && target.charAt(j)=='_') j++;
+        }
+        else if(j>=n){
+            while(i<n && start.charAt(i)=='_') i++;
+        }
+        if(i>=n && j>=n) return true;
+        return false;
     }
 }
