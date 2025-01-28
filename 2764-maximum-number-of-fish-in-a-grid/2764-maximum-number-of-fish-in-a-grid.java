2677e8e55e30abb3f3645b737fc7c6d876b3eeaf
sriraghu42 <127194640+sriraghu42@users.noreply.github.com>
2025-01-27 19:58:22 -0500

    Time: 4 ms (77.03%), Space: 44.9 MB (71.43%) - LeetHub

--- 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.java
+++ 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.java
@@ -1,31 +1,33 @@
 class Solution {
     int[][] directions = new int[][] {{-1,0},{1,0},{0,1},{0,-1}};
-    int ans = 0;
+    int ass = 0;
     public int findMaxFish(int[][] grid) {
+        int k = 0;
         for(int i=0; i<grid.length; i++){
+
             for(int j=0; j<grid[0].length; j++){
                 if(grid[i][j]!=0){
                     //System.out.println("Hi");
-                    int[] count = new int[1];
-                    dfs(grid,i,j,count);
+                    k=Math.max(dfs(grid,i,j),k);
                 } 
             }
         }
-        return ans;
+        return k;
     }
 
-    public void dfs(int[][] grid, int i, int j, int[] count){
-        count[0]+=grid[i][j];
+    public int dfs(int[][] grid, int i, int j){
+        //count[0]+=grid[i][j];
+        int ans = grid[i][j];
         //System.out.println("count : "+count[0]+" , i:"+i+" , j:"+j);
         grid[i][j] = 0;
         for(int[] dir : directions){
             int x = i+dir[0];
             int y = j+dir[1];
             if(x>=grid.length || y>=grid[0].length || x<0 || y<0 || grid[x][y]==0){
-                ans = Math.max(count[0],ans);
-                continue;
+                ans+=0;
             }
-            dfs(grid,x,y,count);
+            else ans+=dfs(grid,x,y);
         }
+        return ans;
     }
 }
