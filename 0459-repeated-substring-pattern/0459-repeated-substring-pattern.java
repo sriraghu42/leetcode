41ecc7d19d5f71eb436d4a86154e9298bed6ec86
sriraghu42 <127194640+sriraghu42@users.noreply.github.com>
2025-02-07 13:53:25 -0500

    Time: 33 ms (76.28%), Space: 45 MB (92.59%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -3,11 +3,13 @@ class Solution {
         int len = 1;
         int length = s.length();
         while(len<=length/2){
-            int j = 0;
-            String sub = s.substring(0,len);
-            StringBuilder sb = new StringBuilder();
-            for(int i=0; i<length/len; i++) sb.append(sub);
-            if(sb.toString().equals(s)) return true;
+            if(length%len==0){
+                int j = 0;
+                String sub = s.substring(0,len);
+                StringBuilder sb = new StringBuilder();
+                for(int i=0; i<length/len; i++) sb.append(sub);
+                if(sb.toString().equals(s)) return true;
+            }
             len++;
         }
         return false;
