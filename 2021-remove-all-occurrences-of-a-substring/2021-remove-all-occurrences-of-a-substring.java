e4039439e84e5052013222a5e6f4e4846e302012
sriraghu42 <127194640+sriraghu42@users.noreply.github.com>
2025-02-11 02:25:55 -0500

    Time: 6 ms (45.15%), Space: 44.3 MB (42.68%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
@@ -1,26 +1,15 @@
 class Solution {
     public String removeOccurrences(String s, String part) {
-        Stack<Character> stack = new Stack<>();
         StringBuilder sb = new StringBuilder();
-        int len = part.length();
+        int targetLength = part.length();
         for(int i=0; i<s.length(); i++){
-           //System.out.println("sb : "+sb.toString());
             char ch = s.charAt(i);
-            stack.push(ch);
-            if(sb.length()==len) sb.deleteCharAt(0);
             sb.append(ch);
-            while(sb.toString().equals(part)){
-                //System.out.println("Inside - sb : "+sb.toString());
-                for(int j=0; j<len; j++) stack.pop();
-                sb.setLength(0);
-                int size = stack.size();
-                int start = 0;
-                if(size>=len) start = size-len;
-                for(int k=start; k<size; k++) sb.append(stack.get(k));
+            while(sb.length()>=targetLength && 
+            sb.substring(sb.length()-targetLength,sb.length()).toString().equals(part)){
+                sb.delete(sb.length()-targetLength,sb.length());
             }
         }
-        sb.setLength(0);
-        for(char ch : stack) sb.append(ch);
         return sb.toString();
     }
 }
