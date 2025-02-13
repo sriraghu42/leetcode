f151a26ff5ce37167abaa58fb3bf2a997e3ac241
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-02-12 20:50:37 -0500

    Time: 151 ms (45.27%), Space: 70.5 MB (80.25%) - LeetHub

--- 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.java
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.java
@@ -1,15 +1,12 @@
 class Solution {
     public int minOperations(int[] nums, int k) {
-        PriorityQueue<Long> pq = new PriorityQueue<>((a,b) -> Long.compare(a,b));
+        PriorityQueue<Long> pq = new PriorityQueue<>();
         for(int num : nums) pq.offer((long) num);
         int counter = 0;
         while(pq.peek()<k){
-            System.out.println(pq.peek());
             counter++;
-            long a = pq.poll();
-            long b = pq.poll();
-            long max=a>=b?a:b;
-            long min=a>=b?b:a;
+            long min = pq.poll();
+            long max = pq.poll();
             pq.offer(min * 2 + max);
         }
         return counter;
