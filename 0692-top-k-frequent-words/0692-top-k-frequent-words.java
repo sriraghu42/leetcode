e2d98dc3c51a534c2eee8aed70d3f98ab90466ae
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-02-13 03:49:41 -0500

    Time: 6 ms (98.63%), Space: 45 MB (23.17%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -1,27 +1,20 @@
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->{
-            if(a.count-b.count!=0) return a.count-b.count;
-            return b.word.compareTo(a.word);
-        });
         Map<String,Integer> map = new HashMap<>();
         for(String word : words) map.put(word,map.getOrDefault(word,0)+1);
+
+        PriorityQueue<Map.Entry<String,Integer>> pq = new PriorityQueue<>((a,b)->{
+            if(a.getValue()- b.getValue()!=0) return a.getValue()- b.getValue();
+            return b.getKey().compareTo(a.getKey());
+        });
+
         for(Map.Entry<String,Integer> entry : map.entrySet()){
-            pq.offer(new Pair(entry.getKey(),entry.getValue()));
+            pq.offer(entry);
             if(pq.size()>k) pq.poll();
         }
         List<String> ans = new ArrayList<>();
-        while(!pq.isEmpty()) ans.add(pq.poll().word);
+        while(!pq.isEmpty()) ans.add(pq.poll().getKey());
         Collections.reverse(ans);
         return ans;
-    }
-}
-
-class Pair{
-    String word;
-    int count;
-    Pair(String word,int count){
-        this.word = word;
-        this.count = count;
     }
 }
