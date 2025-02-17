97001b055fdef914b00ef71b3e4afe683b7b984d
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-02-17 01:03:40 -0500

    Time: 12 ms (54.19%), Space: 45.7 MB (26.01%) - LeetHub

--- 1160-letter-tile-possibilities/1160-letter-tile-possibilities.java
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.java
@@ -1,24 +1,27 @@
 class Solution {
     Set<String> set = new HashSet<>();
     public int numTilePossibilities(String tiles) {
+        char[] charArr = tiles.toCharArray();
+        Arrays.sort(charArr);
         StringBuilder sb = new StringBuilder();
-        int len = tiles.length();
+        int len = charArr.length;
         boolean[] visited = new boolean[len];
-        helperRec(tiles,visited,sb,len);
-        return set.size(); //set.length();
+        helperRec(charArr,visited,sb,len);
+        return set.size();
     }
 
-    public void helperRec(String tiles, boolean[] visited, StringBuilder chosen, int len){
+    public void helperRec(char[] charArr, boolean[] visited, StringBuilder chosen, int len){
         if(chosen.length()==len) return;
         for(int i=0; i<len; i++){
+            if(i>0 && charArr[i]==charArr[i-1] && !visited[i-1]) continue;
             if(!visited[i]){
                 visited[i] = true;
-                char ch = tiles.charAt(i);
+                char ch = charArr[i];
                 chosen.append(ch);
                 String temp = chosen.toString();
                 if(!set.contains(temp)){
                     set.add(temp);
-                    helperRec(tiles,visited,chosen,len);
+                    helperRec(charArr,visited,chosen,len);
                 } 
                 visited[i] = false;
                 chosen.deleteCharAt(chosen.length()-1);
