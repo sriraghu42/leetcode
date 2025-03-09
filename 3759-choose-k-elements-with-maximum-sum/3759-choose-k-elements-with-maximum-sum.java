c8bf95def70b14ccbc5f16d0465406df349167fb
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-03-09 03:35:41 -0400

    Time: 159 ms (100%), Space: 62.2 MB (100%) - LeetHub

--- 3759-choose-k-elements-with-maximum-sum/3759-choose-k-elements-with-maximum-sum.java
+++ 3759-choose-k-elements-with-maximum-sum/3759-choose-k-elements-with-maximum-sum.java
@@ -1,37 +1,44 @@
+import java.util.*;
+
 class Solution {
     public long[] findMaxSum(int[] nums1, int[] nums2, int k) {
-        long[] ans = new long[nums1.length];
-        List<Triple> list = new ArrayList<>();
-        for(int i=0; i<nums1.length; i++){
-            list.add(new Triple(i,nums1[i],nums2[i]));
-        }
-        list.sort((a,b) -> a.i - b.i);
-        //for(Triple triple : list) System.out.println(triple.index + " : " + triple.i +" : " + triple.j);
-        PriorityQueue<Integer> pq = new PriorityQueue<>(k);
-        long sum = 0;
-        for(int m=0; m<list.size(); m++){
-            Triple triple = list.get(m);
-            if(m==0) ;
-            else if(triple.i>list.get(m-1).i){
-                ans[triple.index] = sum;
-            }
-            else ans[triple.index] = ans[list.get(m-1).index];
-            pq.add(triple.j);
-            sum+=triple.j;
-            if(pq.size()>k) sum-=pq.poll();
-            
-        }
-        return ans;
-    }
-}
+        int n = nums1.length;
+        long[] ans = new long[n];
 
-class Triple{
-    int index;
-    int i;
-    int j;
-    Triple(int index, int i, int j){
-        this.index = index;
-        this.i = i;
-        this.j = j;
+        // Step 1: Store (nums1[i], nums2[i], index) and sort by nums1[i]
+        List<int[]> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(new int[]{nums1[i], nums2[i], i});
+        }
+        list.sort(Comparator.comparingInt(a -> a[0])); // Sort by nums1[i] ascending
+
+        // Step 2: Min-Heap to track the largest k values of nums2[j]
+        PriorityQueue<Integer> pq = new PriorityQueue<>(k);
+        long sumHeap = 0;
+        int j = 0; // Pointer to track previous elements
+
+        for (int i = 0; i < n; i++) {
+            int num1_i = list.get(i)[0]; // Current nums1[i]
+            int num2_i = list.get(i)[1]; // Current nums2[i]
+            int index_i = list.get(i)[2]; // Original index
+
+            // Add elements from previous indices nums1[j] < nums1[i]
+            while (j < i && list.get(j)[0] < num1_i) {
+                int num2_j = list.get(j)[1];
+                pq.add(num2_j);
+                sumHeap += num2_j;
+
+                // If more than k elements, remove the smallest
+                if (pq.size() > k) {
+                    sumHeap -= pq.poll();
+                }
+                j++;
+            }
+
+            // Store the max sum if at least one element exists, else 0
+            ans[index_i] = (pq.size() > 0) ? sumHeap : 0;
+        }
+
+        return ans;
     }
 }
