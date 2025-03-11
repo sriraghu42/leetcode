f7766245e6144e9b38751b8c5bf73f897ac8564e
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-03-11 04:51:30 -0400

    Time: 176 ms (32.3%), Space: 45 MB (69.62%) - LeetHub

--- 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
@@ -5,33 +5,29 @@ class Solution {
         sieve[0] = 0;
         sieve[1] = 0;
         for(int i=2; i<right+1; i++){
-            if(sieve[i]==1){
-                for (int k = i * 2; k <= right; k += i) {
-                    sieve[k] = 0;
+            if(sieve[i]!=0){
+                for(int k=2; k*i<=right; k++){
+                    sieve[k*i] = 0;
                 }
             }
         }
+        int[] ans = new int[2];
+        Arrays.fill(ans,-1);
         int prev = -1;
-        int[] ans = new int[] {-1,-1};
-
-        int minDiff = Integer.MAX_VALUE;
+        int min = Integer.MAX_VALUE;
         for(int i=left; i<=right; i++){
             if(sieve[i]==1){
-                if(prev!=-1 && i-prev<minDiff){
-                    minDiff = i-prev;
-                    ans[0] = prev;
-                    ans[1] = i;
-                } 
+                if(prev == -1) ;
+                else{
+                    if(i-prev<min){
+                        ans[0] = prev;
+                        ans[1] = i;
+                        min = i-prev;
+                    }
+                }
                 prev = i;
             }
         }
         return ans;
     }
-
 }
-
-
-//getting all prime numbers???
-
-
-//findding min diff and updating ans in loop
