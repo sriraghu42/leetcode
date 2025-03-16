4289ff905882c1b7141193e8a3edd9c6de921e9a
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-03-16 03:29:33 -0400

    Time: 23 ms (48.03%), Space: 44.9 MB (88.14%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,19 +1,21 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         long i = 1;
-        long j = (long) Math.pow(10,9);
-        while(i<j){
-            long k = i+(j-i)/2;
-            long counter = 0;
+        Arrays.sort(piles);
+        long j = piles[piles.length-1];
+        while(i<=j){
+            long count = 0;
+            long k = (i+j)/2;
+            //System.out.println("k : "+k);
             for(int pile : piles){
-                long a = pile/k;
-                long b = pile%k;
-                if(b>0) counter+=a+1;
-                else counter+=a;
+                long div = pile/k;
+                long rem = pile%k;
+                count+=div+(rem!=0?1:0);
             }
-            if(counter>h) i=k+1;
-            else j=k;
+            //System.out.println("count : "+count);
+            if(count<=h) j=k-1;
+            else i=k+1;
         }
-        return (int)i;
+        return (int) i;
     }
 }
