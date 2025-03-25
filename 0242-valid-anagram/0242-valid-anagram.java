cf9ff57ba97e58f9329b5325d9f8595633ac3a4b
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-03-24 22:14:05 -0400

    Time: 14 ms (29.05%), Space: 44.9 MB (26.62%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+        Map<Character,Integer> map1 = new HashMap<>();
+        Map<Character,Integer> map2 = new HashMap<>();
+        for(char ch : s.toCharArray()){
+            map1.put(ch,map1.getOrDefault(ch,0)+1);
+        }
+        for(char ch : t.toCharArray()){
+            map2.put(ch,map2.getOrDefault(ch,0)+1);
+        }
+        return map1.equals(map2);
+    }
+}
+
+/*
+count of all the letters present in the string
+hashmap and store this count
+map1
+
+count of all the letters present in the string
+hashmap and store this count
+map2
+
+return map1.equals(map2);
+*/
