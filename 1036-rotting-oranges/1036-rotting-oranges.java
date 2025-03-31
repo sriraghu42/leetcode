8a950228c6ef6c74bf36354001556fd2f8aa2934
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-03-31 02:39:37 -0400

    Time: 2 ms (69.87%), Space: 42.7 MB (38.47%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,36 +1,43 @@
 class Solution {
+    int[][] directions = new int[][] {{-1,0},{1,0},{0,-1},{0,1}};
     public int orangesRotting(int[][] grid) {
         int rows = grid.length;
         int cols = grid[0].length;
+        int rotCount = 0;
+        int freshCount = 0;
         Queue<int[]> q = new LinkedList<>();
-        int counter = 0;
-        int freshOranges = 0;
         for(int i=0; i<rows; i++){
             for(int j=0; j<cols; j++){
-                if(grid[i][j]==2) q.offer(new int[] {i,j});
-                if(grid[i][j]==1) freshOranges++;
+                if(grid[i][j] == 1) freshCount++;
+                else if(grid[i][j] == 2){
+                    rotCount++;
+                    q.add(new int[] {i,j});
+                } 
             }
         }
-        if(freshOranges==0) return 0;
-        int[][] directions = new int[][] {{-1,0},{0,1},{1,0},{0,-1}};
-        boolean first = true;
-        while(!q.isEmpty()){
+        if(freshCount==0) return 0;
+        int[] count = new int[] {freshCount};
+        int[] ans = new int[1];
+        bfs(grid,q,count,ans,rows,cols);
+        if(count[0]>0) return -1;
+        return ans[0];
+    }
+
+    public void bfs(int[][] grid, Queue<int[]> q, int[] count, int[] ans, int rows, int cols){
+        while(!q.isEmpty() && count[0]>0){
             int size = q.size();
-            if(!first) counter++;
-            for(int m=0; m<size; m++){
-                int[] cell = q.poll();
-                for(int[] dir : directions){
-                    int a = cell[0] + dir[0];
-                    int b = cell[1] + dir[1];
-                    if(a<0 || a>=rows || b<0 || b>=cols) continue;
-                    if(grid[a][b] == 0 || grid[a][b]==2) continue;
-                    grid[a][b] = 2;
-                    q.add(new int[] {a,b});
-                    freshOranges--;
+            ans[0]++;
+            for(int i=0; i<size; i++){
+                int[] curr = q.poll();
+                for(int[] dir: directions){
+                    int x = curr[0]+dir[0];
+                    int y = curr[1]+dir[1];
+                    if(x<0 || x>=rows || y<0 || y>=cols || grid[x][y]!=1) continue;
+                    grid[x][y]=2;
+                    count[0]--;
+                    q.add(new int[] {x,y});
                 }
-                first = false;
             }
         }
-        return freshOranges==0?counter:-1;    
     }
 }
