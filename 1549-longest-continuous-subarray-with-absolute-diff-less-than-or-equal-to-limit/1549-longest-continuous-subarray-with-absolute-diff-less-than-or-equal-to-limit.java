281723993ea9dd6b5fe0b3aa4dedf5c7e5a7f37e
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-04-15 03:07:44 -0400

    Time: 33 ms (89.61%), Space: 56.3 MB (94.34%) - LeetHub

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -7,15 +7,15 @@ class Solution {
         int j = 0;
         int ans = 0;
         while(j<nums.length){
-            while(i<=j && !dqMin.isEmpty() && !dqMax.isEmpty() && (Math.abs(nums[j]-nums[dqMax.peekFirst()])>limit || Math.abs(nums[j]-nums[dqMin.peekFirst()])>limit)){
-                if(dqMin.peekFirst()==i) dqMin.pollFirst();
-                if(dqMax.peekFirst()==i) dqMax.pollFirst();
-                i++;
-            }
             while(!dqMax.isEmpty() && nums[dqMax.peekLast()]<=nums[j]) dqMax.pollLast();
             dqMax.addLast(j);
             while(!dqMin.isEmpty() && nums[dqMin.peekLast()]>=nums[j]) dqMin.pollLast();
             dqMin.addLast(j);
+            while(i<=j && nums[dqMax.peekFirst()] - nums[dqMin.peekFirst()]>limit){
+                if(dqMin.peekFirst()==i) dqMin.pollFirst();
+                if(dqMax.peekFirst()==i) dqMax.pollFirst();
+                i++;
+            }
             ans = Math.max(ans,j-i+1);
             j++;
         }
