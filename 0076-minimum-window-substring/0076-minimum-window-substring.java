b280bc8cb204e822b653686c8e65e02a814264df
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-04-15 01:21:49 -0400

    Time: 447 ms (5.01%), Space: 45.7 MB (21.29%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,29 +1,30 @@
 class Solution {
-    public boolean isValid(Map<Character,Integer> sMap, Map<Character,Integer> tMap){
-        for(char ch : tMap.keySet()){
-            if(!sMap.containsKey(ch) || sMap.get(ch)<tMap.get(ch)) return false;
-        }
-        return true;
-    }
     public String minWindow(String s, String t) {
-        String ans = "";
-        if(t.length()>s.length()) return ans;
+        Map<Character,Integer> sMap = new HashMap<>();
         Map<Character,Integer> tMap = new HashMap<>();
         for(char ch : t.toCharArray()) tMap.put(ch,tMap.getOrDefault(ch,0)+1);
-        int i = 0;
-        int j = 0;
-        Map<Character,Integer> sMap = new HashMap<>();
-        while(j<s.length()){
-            char ch = s.charAt(j);
-            sMap.put(ch,sMap.getOrDefault(ch,0)+1);
-            while(j-i+1>=t.length() && isValid(sMap,tMap)){
-                if(ans=="" || ans.length()>j-i+1) ans = s.substring(i,j+1);
-                char iCh = s.charAt(i);
-                sMap.put(iCh,sMap.get(iCh)-1);
+        int i=0;
+        int j=0;
+        int len = s.length();
+        String ans = "";
+        while(j<len){
+            char sch = s.charAt(j);
+            sMap.put(sch,sMap.getOrDefault(sch,0)+1);
+            while(checkIfValid(sMap,tMap)){
+                if(ans.equals("") || j-i+1<ans.length()) ans = s.substring(i,j+1);
+                char ich = s.charAt(i);
+                sMap.put(ich,sMap.get(ich)-1);
                 i++;
-            }
+            } 
             j++;
         }
         return ans;
+    }
+
+    public boolean checkIfValid(Map<Character,Integer> sMap, Map<Character,Integer> tMap){
+        for(char tMapKey:tMap.keySet()){
+            if(!sMap.containsKey(tMapKey) || sMap.get(tMapKey)<tMap.get(tMapKey)) return false;
+        }
+        return true;
     }
 }
