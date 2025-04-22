58e55cc86539507c3b711cedfd910dd04a83e7f4
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-04-22 02:44:12 -0400

    Time: 3 ms (8.37%), Space: 47 MB (8.37%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int trap(int[] height) {
+        int i = 0;
+        int j = 1;
+        int ans = 0;
+        while(j<height.length){
+            while(j<height.length && height[j]<height[i]) j++;
+            if(j==height.length) break;
+            for(int m=i+1; m<j; m++) ans+=height[i]-height[m];
+            i=j;
+            j++;
+        }
+        int peak = i;
+        System.out.println(peak);
+        i = height.length-1;
+        j = height.length-2;
+        while(j>peak-1){
+            while(j>peak-1 && height[j]<height[i]) j--;
+            if(j==peak-1) break;
+            for(int m=i-1; m>j; m--) ans+=height[i]-height[m];
+            i=j;
+            j--;
+        }
+        return ans;
+    }
+}
