055a3a84bd86f7bc2e3f79d0f557824d31c667df
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-04-22 02:05:18 -0400

    Time: 17 ms (7.01%), Space: 49.4 MB (79.85%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,28 +1,26 @@
 class Solution {
-    int[][] directions = new int[][] {{-1,0},{1,0},{0,-1},{0,1}};
-
+    int[][] directions = new int[][] {{-1,0},{1,0},{0,1},{0,-1}};
     public int numIslands(char[][] grid) {
-        int rows = grid.length;
-        int cols = grid[0].length;
         int counter = 0;
-        for(int i=0; i<rows; i++){
-            for(int j=0; j<cols; j++){
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++){
                 if(grid[i][j]=='1'){
-                    helperRec(grid,i,j);
+                    System.out.println("Hi");
                     counter++;
+                    dfs(grid,i,j);
                 } 
             }
         }
         return counter;
     }
 
-    public void helperRec(char[][] grid, int i , int j){
-        grid[i][j] = '0';
-        for(int[] dir:directions){
-            int m = i + dir[0];
-            int n = j + dir[1];
-            if(m<0 || n<0 || m>=grid.length || n>=grid[0].length || grid[m][n]=='0') continue;
-            helperRec(grid,m,n);
+    public void dfs(char[][] grid, int i, int j){
+        grid[i][j]='0';
+        for(int[] dir : directions){
+            int x = i+dir[0];
+            int y = j+dir[1];
+            if(x<0 || y<0 || x>=grid.length || y>=grid[0].length || grid[x][y]=='0') continue;
+            dfs(grid,x,y);
         }
     }
 }
