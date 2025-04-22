c9766885ac751e59d512d4ceb960b81e7f7d6285
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-04-22 03:41:59 -0400

    Time: 0 ms (100%), Space: 43.1 MB (76.24%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,33 +1,32 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> ans = new ArrayList<>();
-        StringBuilder chosen = new StringBuilder();
-        chosen.append('(');
-        int openCount = 1;
-        int closedCount = 0;
-        helperRec(n,chosen,ans,openCount,closedCount);
+        int openCount = 0;
+        int closeCount = 0;
+        StringBuilder sb = new StringBuilder();
+        generate(n,openCount,closeCount,sb,ans);
         return ans;
     }
 
-    public void helperRec(int n, StringBuilder chosen, List<String> ans, int openCount, int closedCount){
-        if(chosen.length()==2*n){
-            ans.add(chosen.toString());
+    public void generate(int n, int openCount, int closeCount, StringBuilder sb, List<String> ans){
+        if(sb.length()==2*n){
+            ans.add(sb.toString());
             return;
-        } 
-        //System.out.println(chosen.toString());
-        if(openCount<n){
-            chosen.append('(');
+        }
+        if(openCount!=n){
+            sb.append('(');
             openCount++;
-            helperRec(n,chosen,ans,openCount,closedCount);
-            chosen.deleteCharAt(chosen.length()-1);
+            generate(n,openCount,closeCount,sb,ans);
+            sb.deleteCharAt(sb.length()-1);
             openCount--;
         }
-        if(closedCount<openCount){
-            chosen.append(')');
-            closedCount++;
-            helperRec(n,chosen,ans,openCount,closedCount);
-            chosen.deleteCharAt(chosen.length()-1);
-            closedCount--;
+    
+        if(closeCount<openCount){
+            sb.append(')');
+            closeCount++;
+            generate(n,openCount,closeCount,sb,ans);
+            sb.deleteCharAt(sb.length()-1);
+            closeCount--;
         }
     }
 }
