b668f8816a93f318097e0733690c4c924497dda5
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-04-24 19:13:03 -0400

    Time: 7 ms (89.79%), Space: 44.8 MB (90.26%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.java
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.java
@@ -9,20 +9,12 @@ class Solution {
         int ans = 0;
         while(j<nums.length){
             map.put(nums[j],map.getOrDefault(nums[j],0)+1);
-            if(map.size()==distinct){
-                int k = j+1;
-                int sum = 0;
-                while(k<nums.length && map.containsKey(nums[k])){
-                    sum++;
-                    k++;
-                }
-                while(i<=j && map.size()==distinct){
-                    ans+=sum+1;
-                    map.put(nums[i],map.get(nums[i])-1);
-                    if(map.get(nums[i])==0) map.remove(nums[i]);
-                    i++;
-                } 
-            }
+            while(i<=j && map.size()==distinct){
+                ans+=nums.length-j;
+                map.put(nums[i],map.get(nums[i])-1);
+                if(map.get(nums[i])==0) map.remove(nums[i]);
+                i++;
+            } 
             j++;
         }
         return ans;
