12764342c7e8256db7a02d98f75605c2b79dbce3
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-04-23 22:47:10 -0400

    Time: 5 ms (41.73%), Space: 41.9 MB (35.79%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.java
+++ 0778-reorganize-string/0778-reorganize-string.java
@@ -5,32 +5,22 @@ class Solution {
             map.put(ch,map.getOrDefault(ch,0)+1);
         }
         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->b[1]-a[1]);
-        StringBuilder sb = new StringBuilder();
         for(Map.Entry<Character,Integer> entry : map.entrySet()){
-            pq.add(new int[] {entry.getKey()-'a',entry.getValue()});
+            pq.add(new int[] {(int) entry.getKey(),entry.getValue()});
         }
-        int[] first = new int[2];
-        int[] second = new int[2];
+        StringBuilder sb = new StringBuilder();
         while(!pq.isEmpty()){
-            int[] curr = pq.poll();
-            char ch = (char) (curr[0]+97);
-            if(sb.length()>0 && ch==sb.charAt(sb.length()-1)) return "";
-            sb.append(ch);
-            curr[1]--;
-            first = second;
-            second = curr;
-            if(first[1]!=0) pq.add(first);
-        }
-        System.out.println("String : "+sb.toString());
-        if(first[1]!=0){
-            char ch = (char) (first[0]+97);
-            if(ch==sb.charAt(sb.length()-1)) return "";
-            sb.append(ch);
-        }
-        if(second[1]!=0){
-            char ch = (char) (second[0]+97);
-            if(ch==sb.charAt(sb.length()-1)) return "";
-            sb.append(ch);
+            int[] first = pq.poll();
+            if(sb.length()>0 && sb.charAt(sb.length()-1)==first[0]){
+                if(pq.isEmpty()) return "";
+                int[] second = pq.poll();
+                sb.append((char) second[0]);
+                second[1]--;
+                if(second[1]>0) pq.add(second);
+            }
+            sb.append((char) first[0]);
+            first[1]--;
+            if(first[1]>0) pq.add(first);
         }
         return sb.toString();
     }
