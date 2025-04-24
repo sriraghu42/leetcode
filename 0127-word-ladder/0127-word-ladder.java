858f4b1607c477f31587398d86d47d82de9202a6
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-04-24 19:58:42 -0400

    Time: 73 ms (69.75%), Space: 46.4 MB (65.52%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -4,14 +4,11 @@ class Solution {
         for(String word : wordList) wordSet.add(word);
         if(!wordSet.contains(endWord)) return 0;
         Queue<Pair> q = new LinkedList<>();
-        int level = 1;
         q.add(new Pair(beginWord,1));
         while(!q.isEmpty()){
             int size = q.size();
-            level++;
             for(int i = 0; i<size; i++){
                 Pair p = q.poll();
-                wordSet.remove(p.word);
                 for(int w=0; w<beginWord.length(); w++){
                     StringBuilder sb = new StringBuilder(p.word);
                     char chW = p.word.charAt(w);
@@ -19,8 +16,11 @@ class Solution {
                         if(ch!=chW){
                             sb.setCharAt(w,ch);
                             String newString = sb.toString();
-                            if(newString.equals(endWord)) return level;
-                            if(wordSet.contains(newString)) q.add(new Pair(newString,level));
+                            if(newString.equals(endWord)) return p.num+1;
+                            if(wordSet.contains(newString)){
+                                wordSet.remove(newString);
+                                q.add(new Pair(newString,p.num+1));
+                            } 
                         }
                     }
                 }
