c2b6ef1c9093735aa432f71f1dd551965b4c586f
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-04-25 04:33:12 -0400

    Time: 155 ms (43.22%), Space: 41.9 MB (32.8%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,28 +1,30 @@
 class Solution {
+    int[][] directions = new int[][] {{-1,0},{1,0},{0,1},{0,-1}};
     public boolean exist(char[][] board, String word) {
         int rows = board.length;
         int cols = board[0].length;
         for(int i=0; i<rows; i++){
             for(int j=0; j<cols; j++){
-                char ch = board[i][j];
-                if(helperRec(board,word,0,i,j)) return true;
+                if(board[i][j]==word.charAt(0)){
+                    boolean[][] visited = new boolean[rows][cols];
+                    if(dfs(board,word,i,j,visited,1,rows,cols)) return true;
+                }
             }
         }
         return false;
     }
 
-    public boolean helperRec(char[][] board, String word, int index, int i, int j){
+    public boolean dfs(char[][] board, String word, int i, int j, boolean[][] visited,int index, int rows, int cols){
         if(index==word.length()) return true;
-        if(i<0 || i>board.length-1 || j<0 || j>board[0].length-1) return false;
-        if(board[i][j]=='#') return false;
-        if(board[i][j]==word.charAt(index)){
-            char temp = board[i][j];
-            board[i][j] = '#';
-            boolean ans = helperRec(board,word,index+1,i-1,j) || helperRec(board,word,index+1,i+1,j)
-                    || helperRec(board,word,index+1,i,j-1) || helperRec(board,word,index+1,i,j+1);
-            board[i][j] = temp;
-            return ans;
+        visited[i][j] = true;
+        for(int[] dir : directions){
+            int x = i + dir[0];
+            int y = j + dir[1];
+            if(x<0 || y<0 || x>=rows || y>=cols || visited[x][y] || board[x][y]!=word.charAt(index)) continue;
+            if(dfs(board,word,x,y,visited,index+1,rows,cols)) return true;
+            visited[x][y] = false;
         }
+        visited[i][j] = false;
         return false;
     }
 }
