fb65784011ca406a85703a8e7c0e5b7c8cbda4ad
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-04-29 03:57:15 -0400

    Time: 11 ms (89.68%), Space: 45.5 MB (73.72%) - LeetHub

--- 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.java
@@ -1,30 +1,22 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        StringBuilder sb  = new StringBuilder();
-        Stack<Character> stack = new Stack<>();
-        int j = 0;
-        for(int i=0; i<s.length(); i++){
-            char ch = s.charAt(i);
-            if(ch!='(' && ch!=')') sb.append(ch);
-            else if(ch=='('){
-                j = Math.max(j,i+1);
-                boolean foundClose = false;
-                while(j<s.length()){
-                    if(s.charAt(j)==')'){
-                        foundClose = true;
-                        break;
-                    }
-                    j++;
-                }
-                if(foundClose){
-                    sb.append(ch);
-                    stack.push(ch);
-                    j++;
-                }
-            }
-            else{
-                if(!stack.isEmpty() && stack.pop()=='(') sb.append(ch);
-            }
+        int openCount = 0;
+        int closeCount = 0;
+        StringBuilder sb = new StringBuilder();
+        for(char ch : s.toCharArray()){
+            if(ch=='(') openCount++;
+            else if(ch==')' && openCount>closeCount) closeCount++;
+        }
+        int reqCount = Math.min(openCount,closeCount);
+        //System.out.println(reqCount);
+        openCount = 0;
+        closeCount = 0;
+        for(char ch : s.toCharArray()){
+            if(ch=='(' && openCount>=reqCount) continue;
+            if(ch==')' && closeCount==openCount) continue;
+            if(ch=='(') openCount++;
+            else if(ch==')') closeCount++;
+            sb.append(ch);
         }
         return sb.toString();
     }
