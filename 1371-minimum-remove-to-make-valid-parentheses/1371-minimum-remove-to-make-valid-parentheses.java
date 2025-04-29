1def606129d57d6d5548489f03ddefa0b0b68d8f
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-04-29 04:12:01 -0400

    Time: 24 ms (38.06%), Space: 45.4 MB (85.16%) - LeetHub

--- 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.java
@@ -1,22 +1,19 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        int openCount = 0;
-        int closeCount = 0;
-        StringBuilder sb = new StringBuilder();
-        for(char ch : s.toCharArray()){
-            if(ch=='(') openCount++;
-            else if(ch==')' && openCount>closeCount) closeCount++;
+        Set<Integer> set = new HashSet<>();
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch=='(') stack.push(i);
+            else if(ch==')'){
+                if(!stack.isEmpty()) stack.pop();
+                else set.add(i);
+            }
         }
-        int reqCount = Math.min(openCount,closeCount);
-        //System.out.println(reqCount);
-        openCount = 0;
-        closeCount = 0;
-        for(char ch : s.toCharArray()){
-            if(ch=='(' && openCount>=reqCount) continue;
-            if(ch==')' && closeCount==openCount) continue;
-            if(ch=='(') openCount++;
-            else if(ch==')') closeCount++;
-            sb.append(ch);
+        while(!stack.isEmpty()) set.add(stack.pop());
+        StringBuilder sb  = new StringBuilder();
+        for(int i=0; i<s.length(); i++){
+            if(!set.contains(i)) sb.append(s.charAt(i));
         }
         return sb.toString();
     }
