022b9dc80beaf390452058906a68cf5422af30b6
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-05-13 03:31:40 -0400

    Time: 63 ms (28.34%), Space: 45.7 MB (45.56%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.java
+++ 0876-hand-of-straights/0876-hand-of-straights.java
@@ -3,24 +3,15 @@ class Solution {
         int len = hand.length;
         if(len%groupSize!=0) return false;
         int groups = len/groupSize;
-        Map<Integer,Integer> map = new HashMap<>();
+        TreeMap<Integer,Integer> map = new TreeMap<>();
         for(int num : hand) map.put(num,map.getOrDefault(num,0)+1);
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0]-b[0]);
-        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
-            pq.add(new int[] {entry.getKey(),entry.getValue()});
-        }
-        while(groups>0){
-            groups--;
-            int[][] groupHand = new int[groupSize][2];
+        while(!map.isEmpty()){
+            int small = map.firstKey();
             for(int i=0; i<groupSize; i++){
-                if(pq.isEmpty()) return false;
-                int[] temp = pq.poll();
-                groupHand[i][0]=temp[0];
-                groupHand[i][1]=temp[1];
-            }
-            for(int i=0; i<groupSize; i++){
-                if(groupHand[i][1]>1) pq.add(new int[] {groupHand[i][0],groupHand[i][1]-1});
-                if(i>0 && groupHand[i][0]!=groupHand[i-1][0]+1) return false;
+                if(!map.containsKey(small)) return false;
+                map.put(small,map.get(small)-1);
+                if(map.get(small)==0) map.remove(small);
+                small++;
             }
         }
         return true;
