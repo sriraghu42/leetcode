ddb47d5026291210bf0dded9cfbe2ff7d505e9b6
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-05-12 22:44:36 -0400

    Time: 364 ms (16.61%), Space: 44.4 MB (98.25%) - LeetHub

--- 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.java
+++ 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.java
@@ -1,45 +1,32 @@
 class Solution {
     public int[] findEvenNumbers(int[] digits) {
         Set<Integer> set = new HashSet<>();
-        int n = digits.length;
-        for(int i=0; i<n; i++){
-            if(digits[i]==0) continue;
-            int sum1 = digits[i];
-            for(int j=0; j<n; j++){
-                if(j==i) continue;
-                int sum2 = sum1*10+digits[j];
-                for(int k=0; k<n; k++){
-                    if(k==i || k==j || digits[k]%2!=0) continue;
-                    int sum3 = sum2*10+digits[k];
-                    set.add(sum3);
-                }
-            }
-        }
+        int len = digits.length;
+        int curr = 0;
+        Arrays.sort(digits);
+        boolean[] visited = new boolean[digits.length];
+        search(digits,len,set,curr,visited);
         List<Integer> list = new ArrayList<>(set);
         list.sort(null);
         int[] ans = new int[list.size()];
-        for(int i=0; i<list.size(); i++){
+        for(int i=0; i<ans.length; i++){
             ans[i] = list.get(i);
         }
         return ans;
     }
+
+    public void search(int[] digits, int len, Set<Integer> set, int curr, boolean[] visited){
+        if(curr>99){
+            if(curr%2==0) set.add(curr);
+            return;
+        }
+        for(int i=0; i<len; i++){
+            if(curr==0 && digits[i]==0) continue;
+            if(!visited[i]){
+                visited[i] = true;
+                search(digits,len,set,curr*10+digits[i],visited);
+                visited[i] = false;
+            }
+        }
+    }
 }
-
-/*
-2 1
-
-
-2
-1 3
-1 0
-3 1
-3 0
-
-0
-2 1
-2 3
-1 2
-1 3
-3 2
-3 1
-*/
