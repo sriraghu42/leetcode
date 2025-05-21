313af8eb576602f259592a77cc1a249b8a1b53e9
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-05-21 15:00:36 -0400

    Time: 1 ms (93.92%), Space: 42.3 MB (16.98%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.java
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -4,14 +4,24 @@ class Solution {
         int len = s.length();
         StringBuilder sb = new StringBuilder();
         Set<String> set = new HashSet<>(wordDict);
-        search(s,len,set,sb,0);
+        Map<Integer,List<String>> map = new HashMap<>();
+        search(s,len,set,sb,0,map);
         return ans;
     }
 
-    public void search(String s, int len, Set<String> set, StringBuilder sb, int index){
+    public void search(String s, int len, Set<String> set, StringBuilder sb, int index, Map<Integer,List<String>> map){
         if(index==len){
             sb.setLength(sb.length()-1);
             ans.add(sb.toString());
+            return;
+        }
+        if(map.containsKey(index)){
+            int currLen = sb.length();
+            for(String str : map.get(index)){
+                sb.append(str).append(" ");
+                search(s,len,set,sb,index+str.length(),map);
+                sb.setLength(currLen);
+            }
             return;
         }
         for(int i=index; i<len; i++){
@@ -19,7 +29,13 @@ class Solution {
             if(set.contains(word)){
                 int currLen = sb.length();
                 sb.append(word).append(" ");
-                search(s,len,set,sb,i+1);
+                List<String> list = new ArrayList<>();
+                if(map.containsKey(index)){
+                    list = map.get(index);
+                }
+                else map.put(index,list);
+                list.add(word);
+                search(s,len,set,sb,i+1,map);
                 sb.setLength(currLen);
             }
         }
