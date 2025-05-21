c7b9eaa3a95f5ed400519c52397d011e8e4da6d1
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-05-21 14:26:07 -0400

    Time: 1 ms (99.58%), Space: 42.8 MB (54.17%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.java
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -2,28 +2,25 @@ class Solution {
     List<String> ans = new ArrayList<>();
     public List<String> restoreIpAddresses(String s) {
         StringBuilder sb = new StringBuilder();
-        backtrack(s,sb,0,0);
+        int len = s.length();
+        search(s,len,sb,0,0);
         return ans;
     }
 
-    public void backtrack(String s, StringBuilder sb, int index,int counter){
-        if(counter==4 && index==s.length()){
-            sb.deleteCharAt(sb.length()-1);
+    public void search(String s, int len, StringBuilder sb, int index, int counter){
+        if(counter==4 && index!=len) return;
+        if(counter==4 && index==len){
+            sb.setLength(sb.length()-1);
             ans.add(sb.toString());
             return;
-        } 
-        else if(counter==4 || index==s.length()) return;
-        for(int i=index; i<s.length(); i++){
-            if(i>index && s.charAt(index)=='0') return;
-            int sum=Integer.parseInt(s.substring(index,i+1));
-            if(sum>255) return;
-            int lenBefore = sb.length();
-            sb.append(sum).append('.');
-            backtrack(s,sb,i+1,counter+1);
-            //System.out.println("sb:"+sb);
-            //System.out.println("i:"+i);
-            //System.out.println("index"+index);
-            sb.setLength(lenBefore);
+        }
+        for(int i=index; i<len; i++){
+            int num = Integer.parseInt(s.substring(index,i+1));
+            if(num>255 || String.valueOf(num).length()!=i-index+1) return;
+            int currLen = sb.length();
+            sb.append(num).append('.');
+            search(s,len,sb,i+1,counter+1);
+            sb.setLength(currLen);
         }
     }
 }
