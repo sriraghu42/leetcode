fc624c1f3aa92bbfd0c1a775d556c8bd1c8126ad
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-05-21 14:37:53 -0400

    Time: 7 ms (50.31%), Space: 44.6 MB (46.5%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,22 +1,20 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> set = new HashSet<>(wordDict);
         int len = s.length();
-        Set<Integer> visited = new HashSet<>();
-        return search(s,set,0,len,visited);
+        Set<String> set = new HashSet<>(wordDict);
+        Set<Integer> failedSet = new HashSet<>();
+        return search(s,len,set,0,failedSet);
     }
 
-    public boolean search(String s, Set<String> set,int index, int len, Set<Integer> visited){
-        if(visited.contains(index)) return false;
-        if(index == len) return true;
-        StringBuilder temp = new StringBuilder();
+    public boolean search(String s, int len, Set<String> set, int index, Set<Integer> failedSet){
+        if(failedSet.contains(index)) return false;
+        if(index==len) return true;
         for(int i=index; i<len; i++){
-            temp.append(s.charAt(i));
-            if(set.contains(temp.toString())){
-                if(search(s,set,i+1,len,visited)) return true;
-                else visited.add(i+1);
+            if(set.contains(s.substring(index,i+1))){
+                if(search(s,len,set,i+1,failedSet)) return true;
             }
         }
+        failedSet.add(index);
         return false;
     }
 }
