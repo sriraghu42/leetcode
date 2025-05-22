4e2d8d386434664f5075c1039393e226ba59026d
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-05-22 19:51:59 -0400

    Time: 3 ms (84.24%), Space: 49.8 MB (45.86%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -7,29 +7,22 @@ class Solution {
         for(int i=0; i<rows; i++){
             for(int j=0; j<cols; j++){
                 if(grid[i][j]=='1'){
-                    bfs(grid,i,j,rows,cols);
+                    dfs(grid,i,j,rows,cols);
                     ans++;
-                }
+                } 
             }
         }
         return ans;
     }
 
-    public void bfs(char[][] grid, int i, int j, int rows, int cols){
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[] {i,j});
+
+    public void dfs(char[][] grid, int i, int j, int rows, int cols){
         grid[i][j]='0';
-        while(!q.isEmpty()){
-            int[] arr = q.poll();
-            i = arr[0];
-            j = arr[1];
-            for(int[] dir : directions){
-                int x = i+dir[0];
-                int y = j+dir[1];
-                if(x<0 || x>=rows || y<0 || y>=cols || grid[x][y]=='0') continue;
-                grid[x][y] = '0';
-                q.add(new int[] {x,y});
-            }
+        for(int[] dir : directions){
+            int x = i+dir[0];
+            int y = j+dir[1];
+            if(x<0 || x>=rows || y<0 || y>=cols || grid[x][y]=='0') continue;
+            dfs(grid,x,y,rows,cols);
         }
     }
 }
