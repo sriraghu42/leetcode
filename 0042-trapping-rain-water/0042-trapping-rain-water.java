88a2c20471937f1fa7a91d8cf7d4e9c1e9b748cc
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-05-22 20:39:16 -0400

    Time: 1 ms (62.81%), Space: 46.7 MB (28.59%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,24 +1,36 @@
 class Solution {
     public int trap(int[] height) {
         int i = 0;
-        int j = 1;
+        int len = height.length;
         int ans = 0;
-        while(j<height.length){
-            while(j<height.length && height[j]<height[i]) j++;
-            if(j==height.length) break;
-            for(int m=i+1; m<j; m++) ans+=height[i]-height[m];
+        int end = -1;
+        while(i<len){
+            int j = i+1;
+            while(j<len && height[j]<height[i]) j++;
+            if(j==len){
+                end = i;
+                break;
+            }
+            int compValue = height[i];
+            while(i<j){
+                ans+=compValue - height[i];
+                i++;
+            }
             i=j;
-            j++;
         }
-        int peak = i;
-        i = height.length-1;
-        j = height.length-2;
-        while(j>peak-1){
-            while(j>peak-1 && height[j]<height[i]) j--;
-            if(j==peak-1) break;
-            for(int m=i-1; m>j; m--) ans+=height[i]-height[m];
-            i=j;
-            j--;
+        if(end!=-1){
+            i=len-1;
+            while(i>=end){
+                int j = i-1;
+                while(j>=end && height[j]<height[i]) j--;
+                if(j<end) break;
+                int compValue = height[i];
+                while(i>j){
+                    ans+=compValue - height[i];
+                    i--;
+                }
+                i=j;
+            }
         }
         return ans;
     }
