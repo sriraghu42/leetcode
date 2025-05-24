d79116debaff651c0bdca6cf4ff0f421e5d79b05
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-05-24 02:35:30 -0400

    Time: 32 ms (14.78%), Space: 45.6 MB (43.61%) - LeetHub

--- 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.java
@@ -1,20 +1,29 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        Set<Integer> set = new HashSet<>();
-        Stack<Integer> stack = new Stack<>();
-        for(int i=0; i<s.length(); i++){
+        Stack<Pair> stack = new Stack<>();
+        for(int i = 0; i<s.length(); i++){
             char ch = s.charAt(i);
-            if(ch=='(') stack.push(i);
+            if(ch=='(') stack.push(new Pair('(',i));
             else if(ch==')'){
-                if(!stack.isEmpty()) stack.pop();
-                else set.add(i);
+                if(stack.isEmpty() || stack.peek().ch==')') stack.push(new Pair(')',i));
+                else stack.pop();
             }
         }
-        while(!stack.isEmpty()) set.add(stack.pop());
-        StringBuilder sb  = new StringBuilder();
+        Set<Integer> set = new HashSet<>();
+        for(Pair p : stack) set.add(p.index);
+        StringBuilder sb = new StringBuilder();
         for(int i=0; i<s.length(); i++){
             if(!set.contains(i)) sb.append(s.charAt(i));
         }
         return sb.toString();
+    }
+}
+
+class Pair{
+    char ch;
+    int index;
+    Pair(char ch, int index){
+        this.ch = ch;
+        this.index = index;
     }
 }
