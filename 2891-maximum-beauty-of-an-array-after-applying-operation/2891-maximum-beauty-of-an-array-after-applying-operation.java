ed5ebef4bf57d32e2a9bd42fca3a144727ce8a42
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-05-26 15:35:21 -0400

    Time: 659 ms (5.15%), Space: 57.7 MB (92.89%) - LeetHub

--- 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.java
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.java
@@ -1,17 +1,28 @@
 class Solution {
     public int maximumBeauty(int[] nums, int k) {
+        TreeMap<Integer,Integer> map = new TreeMap<>();
+        for(int num : nums){
+            map.put(num-k, map.getOrDefault(num-k,0)+1);
+            map.put(num+k+1, map.getOrDefault(num+k+1,0)-1);
+        }
+        int count = 0;
         int ans = 0;
-        Arrays.sort(nums);
-        int i = 0;
-        int j = 0;
-        int len = nums.length;
-        while(j<len){
-            while(nums[j]-nums[i]>2*k){
-                i++;
-            }
-            ans = Math.max(ans,j-i+1);
-            j++;
+        for(int value : map.values()){
+            count+=value;
+            ans = Math.max(ans,count);
         }
         return ans;
     }
 }
+
+/*
+1 3
+0 2
+2 4
+0 1 0 0 -1 0
+1 0 0 -1 0 0
+0 0 1 0  1 -1
+
+1 1 1 -1 0 -1
+1 2 3 2 2 3
+*/
