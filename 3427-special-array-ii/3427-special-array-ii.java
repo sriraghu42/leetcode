cf98592d6a35aed5ae6c494129ae245cf16ddcd3
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-05-27 05:14:56 -0400

    Time: 3 ms (88.8%), Space: 98.9 MB (27.48%) - LeetHub

--- 3427-special-array-ii/3427-special-array-ii.java
+++ 3427-special-array-ii/3427-special-array-ii.java
@@ -1,17 +1,15 @@
 class Solution {
     public boolean[] isArraySpecial(int[] nums, int[][] queries) {
-        int[] count = new int[nums.length];
-        count[0] = 0;
+        int[] pf = new int[nums.length];
         int counter = 0;
         for(int i=1; i<nums.length; i++){
-            if(nums[i]%2==nums[i-1]%2) counter++;
-            count[i] = counter;
+            if(Math.abs(nums[i]-nums[i-1])%2!=0) counter++;
+            pf[i] = counter;
         }
         boolean[] ans = new boolean[queries.length];
         for(int i=0; i<queries.length; i++){
             int[] query = queries[i];
-            if(count[query[1]]-count[query[0]]==0) ans[i] = true;
-            else ans[i] = false;
+            if(pf[query[1]]-pf[query[0]]==query[1]-query[0]) ans[i] = true;
         }
         return ans;
     }
