0de66bc7e88edd6ead125c15c88adc4ddc3fbcbd
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-05-27 15:09:55 -0400

    Time: 11 ms (97.42%), Space: 43.1 MB (26.54%) - LeetHub

--- 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.java
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.java
@@ -8,48 +8,32 @@
  * }
  */
 class Solution {
+    Map<TreeNode,TreeNode> map;
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-        Map<TreeNode,TreeNode> map = new HashMap<>();
-        dfs(map,root);
+        map = new HashMap<>();
+        dfs(root,null);
         Set<TreeNode> set = new HashSet<>();
-        set.add(target);
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(target);
-        while(!q.isEmpty() && k>0){
-            int len = q.size();
-            //System.out.println("************");
-            for(int i=0; i<len; i++){
-                TreeNode node = q.poll();
-                //System.out.println(node.val);
-                if(node.left!=null && !set.contains(node.left)){
-                    q.add(node.left);
-                    set.add(node.left);
-                } 
-                if(node.right!=null && !set.contains(node.right)){
-                    q.add(node.right);
-                    set.add(node.right);
-                } 
-                if(map.containsKey(node) && !set.contains(map.get(node))){
-                    q.add(map.get(node));
-                    set.add(map.get(node));
-                } 
-            }
-            k--;
-        }
-        List<Integer> ans = new ArrayList<>();
-        while(!q.isEmpty()) ans.add(q.poll().val);
-        return ans;
+        List<Integer> list = new ArrayList<>();
+        depthSearch(target,k,list,set);
+        return list;
     }
 
-    private void dfs(Map<TreeNode,TreeNode> map, TreeNode root){
+    private void depthSearch(TreeNode root, int k, List<Integer> list, Set<TreeNode> set){
+        if(root==null || set.contains(root)) return;
+        set.add(root);
+        if(k==0){
+            list.add(root.val);
+            return;
+        } 
+        depthSearch(root.left,k-1,list,set);
+        depthSearch(root.right,k-1,list,set);
+        depthSearch(map.get(root),k-1,list,set);
+    }
+
+    private void dfs(TreeNode root, TreeNode parent){
         if(root == null) return;
-        if(root.left!=null){
-            map.put(root.left,root);
-            dfs(map,root.left);
-        }
-        if(root.right!=null){
-            map.put(root.right,root);
-            dfs(map,root.right);
-        }
+        map.put(root,parent);
+        dfs(root.left,root);
+        dfs(root.right,root);
     }
 }
