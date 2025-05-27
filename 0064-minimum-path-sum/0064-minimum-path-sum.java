80e639913cbb29a40fa991e9adf58ed9ec2ecee2
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-05-27 19:39:31 -0400

    Time: 1 ms (99.86%), Space: 47.6 MB (69.87%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -3,6 +3,7 @@ class Solution {
         int rowCount = grid.length;
         int colCount = grid[0].length;
         int[][] dp = new int[rowCount][colCount];
+        /*
         int sum = 0;
         for(int col=0; col<colCount;col++){
             sum+=grid[0][col];
@@ -13,11 +14,13 @@ class Solution {
             sum+=grid[row][0];
             dp[row][0] = sum;
         }
+        */
         return helper(grid,rowCount-1,colCount-1,dp);
     }
 
     private int helper(int[][] grid, int row, int col, int[][] dp){
-        if(row<0 || col<0) return 0;
+        if(row==0 && col==0) return grid[row][col];
+        if(row<0 || col<0) return Integer.MAX_VALUE;
         if(dp[row][col]!=0) return dp[row][col];
         dp[row][col] = grid[row][col]+Math.min(helper(grid, row, col-1, dp) , helper(grid,row-1,col,dp));
         return dp[row][col];
