0d3bcc342a451416ad8fb12ef7235cbf2cdbfdf3
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-05-31 04:44:44 -0400

    Time: 31 ms (41.02%), Space: 57.7 MB (67.07%) - LeetHub

--- 2868-continuous-subarrays/2868-continuous-subarrays.java
+++ 2868-continuous-subarrays/2868-continuous-subarrays.java
@@ -1,19 +1,20 @@
 class Solution {
     public long continuousSubarrays(int[] nums) {
-        long counter = 0;
+        TreeMap<Integer,Integer> map = new TreeMap<>();
         int i = 0;
         int j = 0;
-        TreeMap<Integer,Integer> treeMap = new TreeMap<>();
-        while(j<nums.length){
-            treeMap.put(nums[j],treeMap.getOrDefault(nums[j],0)+1);
-            while(treeMap.lastEntry().getKey() - treeMap.firstEntry().getKey()>2){
-                treeMap.put(nums[i],treeMap.get(nums[i])-1);
-                if(treeMap.get(nums[i])==0) treeMap.remove(nums[i]);
+        int len = nums.length;
+        long ans = 0;
+        while(j<len){
+            map.put(nums[j],map.getOrDefault(nums[j],0)+1);
+            while(Math.abs(map.firstKey()-map.lastKey())>2){
+                map.put(nums[i],map.get(nums[i])-1);
+                if(map.get(nums[i])==0) map.remove(nums[i]);
                 i++;
             }
-            counter+=j-i+1;
+            ans+=j-i+1;
             j++;
         }
-        return counter;
+        return ans;
     }
 }
