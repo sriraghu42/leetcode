361bcf8c200ba81f2adc176ba93c05e82a65a26e
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-05-31 04:53:56 -0400

    Time: 18 ms (86.68%), Space: 62 MB (7.49%) - LeetHub

--- 2868-continuous-subarrays/2868-continuous-subarrays.java
+++ 2868-continuous-subarrays/2868-continuous-subarrays.java
@@ -1,16 +1,20 @@
 class Solution {
     public long continuousSubarrays(int[] nums) {
-        TreeMap<Integer,Integer> map = new TreeMap<>();
+        Deque<Integer> maxDq = new ArrayDeque<>();
+        Deque<Integer> minDq = new ArrayDeque<>();
         int i = 0;
         int j = 0;
-        int len = nums.length;
         long ans = 0;
+        int len = nums.length;
         while(j<len){
-            map.put(nums[j],map.getOrDefault(nums[j],0)+1);
-            while(Math.abs(map.firstKey()-map.lastKey())>2){
-                map.put(nums[i],map.get(nums[i])-1);
-                if(map.get(nums[i])==0) map.remove(nums[i]);
+            while(!maxDq.isEmpty() && nums[maxDq.peekLast()]<=nums[j]) maxDq.pollLast();
+            while(!minDq.isEmpty() && nums[minDq.peekLast()]>=nums[j]) minDq.pollLast();
+            maxDq.addLast(j);
+            minDq.addLast(j);
+            while(nums[maxDq.peekFirst()]-nums[minDq.peekFirst()]>2){
                 i++;
+                while(!minDq.isEmpty() && minDq.peekFirst()<i) minDq.pollFirst();
+                while(!maxDq.isEmpty() && maxDq.peekFirst()<i) maxDq.pollFirst();
             }
             ans+=j-i+1;
             j++;
