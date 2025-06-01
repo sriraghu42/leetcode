9608028eeacc4c487db27c6ab0b41b72aa3b2936
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-06-01 03:27:27 -0400

    Time: 1060 ms (7.88%), Space: 47 MB (14.2%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
@@ -3,24 +3,19 @@ class Solution {
         Set<String> set = new HashSet<>();
         Set<Character> visited = new HashSet<>();
         int len = s.length();
-        StringBuilder sb = new StringBuilder();
+        int[] lastPos = new int[26];
+        Arrays.fill(lastPos,-1);
+        for(int i=len-1; i>=0; i--){
+            char ich = s.charAt(i);
+            if(lastPos[ich-'a']!=-1) continue;
+            lastPos[ich-'a']=i;
+        }
         for(int i=0; i<len; i++) {
             char ich = s.charAt(i);
             if(visited.contains(ich)) continue;
             visited.add(ich);
-            int j = len-1;
-            while(j>i){
-                char jch = s.charAt(j);
-                if(ich==jch){
-                    for(int c=i+1; c<j; c++) {
-                        sb.append(ich).append(s.charAt(c)).append(jch);
-                        set.add(sb.toString());
-                        sb.setLength(0);
-                    }
-                    break;
-                }
-                j--;
-            }
+            int j = lastPos[ich-'a'];
+            for(int c=i+1; c<j; c++) set.add(""+ich+s.charAt(c)+ich);
         }
         return set.size();
     }
