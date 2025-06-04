fea8cf67c35ef815f9fa77ae0e5e3736267c5c9e
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-06-04 02:34:07 -0400

    Time: 5 ms (96.28%), Space: 42.7 MB (79.83%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,23 +1,30 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
+        int len1 = s1.length();
+        int len2 = s2.length();
+        if(len2<len1) return false;
         int[] a = new int[26];
-        int[] b  =new int[26];
         for(char ch : s1.toCharArray()){
             a[ch-'a']++;
         }
         int i = 0;
         int j = 0;
-        int len1 = s1.length();
-        int len2 = s2.length();
         while(j<len2){
-            b[s2.charAt(j)-'a']++;
+            a[s2.charAt(j)-'a']--;
             if(j-i+1==len1){
-                if(Arrays.equals(a,b)) return true;
-                b[s2.charAt(i)-'a']--;
+                if(isValid(a)) return true;
+                a[s2.charAt(i)-'a']++;
                 i++;
             }  
             j++;
         }
         return false;
+    }
+
+    public boolean isValid(int[] arr){
+        for(int num : arr){
+            if(num!=0) return false;
+        }
+        return true;
     }
 }
