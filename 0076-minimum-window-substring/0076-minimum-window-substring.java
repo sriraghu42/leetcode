10a712b312b1aff594196a2d4cdf6efa3a2c4a4b
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-06-04 22:52:01 -0400

    Time: 606 ms (5.01%), Space: 46 MB (8.15%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -3,27 +3,31 @@ class Solution {
         Map<Character,Integer> sMap = new HashMap<>();
         Map<Character,Integer> tMap = new HashMap<>();
         for(char ch : t.toCharArray()) tMap.put(ch,tMap.getOrDefault(ch,0)+1);
-        int i=0;
-        int j=0;
+        int i = 0;
+        int j = 0;
         int len = s.length();
         String ans = "";
         while(j<len){
-            char sch = s.charAt(j);
-            sMap.put(sch,sMap.getOrDefault(sch,0)+1);
-            while(checkIfValid(sMap,tMap)){
-                if(ans.equals("") || j-i+1<ans.length()) ans = s.substring(i,j+1);
+            char jch = s.charAt(j);
+            sMap.put(jch,sMap.getOrDefault(jch,0)+1);
+            while(isValid(sMap,tMap)){
+                if(ans.length()==0 || j-i+1<ans.length()){
+                    System.out.println("Hi");
+                    ans = s.substring(i,j+1); 
+                } 
                 char ich = s.charAt(i);
                 sMap.put(ich,sMap.get(ich)-1);
+                if(sMap.get(ich)==0) sMap.remove(ich);
                 i++;
-            } 
+            }
             j++;
         }
         return ans;
     }
 
-    public boolean checkIfValid(Map<Character,Integer> sMap, Map<Character,Integer> tMap){
-        for(char tMapKey:tMap.keySet()){
-            if(!sMap.containsKey(tMapKey) || sMap.get(tMapKey)<tMap.get(tMapKey)) return false;
+    public boolean isValid(Map<Character,Integer> sMap,Map<Character,Integer> tMap){
+        for(char key : tMap.keySet()){
+            if(!sMap.containsKey(key) || sMap.get(key)<tMap.get(key)) return false;
         }
         return true;
     }
