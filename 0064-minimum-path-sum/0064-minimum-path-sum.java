d0ba7a8acbc641a4971d53614469e1091083d01c
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-06-11 06:02:19 -0400

    Time: 4 ms (63.11%), Space: 47.5 MB (81.12%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -3,16 +3,14 @@ class Solution {
         int rowCount = grid.length;
         int colCount = grid[0].length;
         int[][] dp = new int[rowCount][colCount];
-        for(int[] arr : dp) Arrays.fill(arr,-1);
         dp[0][0] = grid[0][0];
-        findMin(grid,rowCount-1,colCount-1,rowCount,colCount,dp);
+        for(int i=1; i<colCount; i++) dp[0][i] = grid[0][i]+dp[0][i-1];
+        for(int i=1; i<rowCount; i++) dp[i][0] = grid[i][0]+dp[i-1][0];
+        for(int i=1; i<rowCount; i++){
+            for(int j=1; j<colCount; j++){
+                dp[i][j] = grid[i][j] + Math.min(dp[i][j-1],dp[i-1][j]);
+            }
+        }
         return dp[rowCount-1][colCount-1];
-    }
-
-    public int findMin(int[][] grid, int row, int col, int rowCount, int colCount, int[][] dp){
-        if(row<0 || col<0) return Integer.MAX_VALUE;
-        if(dp[row][col]>-1) return dp[row][col];
-        dp[row][col] = Math.min(findMin(grid,row,col-1,rowCount,colCount,dp),findMin(grid,row-1,col,rowCount,colCount,dp))+grid[row][col];
-        return dp[row][col];
     }
 }
