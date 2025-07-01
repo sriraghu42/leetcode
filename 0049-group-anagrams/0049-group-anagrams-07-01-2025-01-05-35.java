969629487ed17c6ab6a98db0ae39e88c09d26432
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2025-07-01 01:05:35 -0400

    Time: 7 ms (65.74%), Space: 48 MB (55.92%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams-07-01-2025-01-05-35.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> ans = new ArrayList<>();
+        Map<String,List<String>> map = new HashMap<>();
+        for(String str : strs){
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+            String sortedStr = new String(arr);
+            if(map.containsKey(sortedStr)){
+                map.get(sortedStr).add(str);
+            }
+            else{
+                List<String> list = new ArrayList<>();
+                list.add(str);
+                map.put(sortedStr,list);
+            }
+        }
+        for(List<String> list: map.values()) ans.add(list);
+        return ans;
+    }
+}
