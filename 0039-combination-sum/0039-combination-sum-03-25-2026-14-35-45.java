2b9f1cfaf97b9a115274fc2e3bee4a23dc60ee0c
Sri Raghu Katragadda <katragaddasriraghu@gmail.com>
2026-03-25 14:35:46 -0400

    Time: 3 ms (35.18%), Space: 46.1 MB (15.85%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum-03-25-2026-14-35-45.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        int len = candidates.length;
+        Arrays.sort(candidates);
+        List<Integer> chosen = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        backtrack(candidates,target,len,chosen,ans,0);
+        return ans;
+    }
+
+    public void backtrack(int[] candidates, int target, int len, List<Integer> chosen, List<List<Integer>> ans, int pos){
+        if(target==0){
+            ans.add(new ArrayList<>(chosen));
+        }
+        for(int i=pos;i<len;i++){
+            if(target-candidates[i]<0) break;
+            chosen.add(candidates[i]);
+            backtrack(candidates,target-candidates[i],len,chosen,ans,i);
+            chosen.remove(chosen.size()-1);
+        }
+    }
+}
